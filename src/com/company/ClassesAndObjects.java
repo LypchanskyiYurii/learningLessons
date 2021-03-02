a60f8cff337a90af6bbc20d8d85ed6802014d630
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-02 13:27:47 +0200

    Change ClassesAndObjects.java, add method calculateYearsRetirement

--- src/com/company/ClassesAndObjects.java
+++ src/com/company/ClassesAndObjects.java
@@ -4,9 +4,11 @@ public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
         person1.name = "Вова";
-        person1.age = 80;
+        person1.age = 55;
         person1.speak();
         person1.sayHello();
+        int years1 = person1.calculateYearsRetirement();
+        System.out.println("Мені залишилось до пенсї " + years1);
 
         System.out.println("___________________________________");
         Person person2 = new Person();
@@ -14,6 +16,8 @@ public class ClassesAndObjects {
         person2.age = 30;
         person2.speak();
         person2.sayHello();
+        int years2 = person2.calculateYearsRetirement();
+        System.out.println("Мені залишилось до пенсї " + years2);
 
     }
 }
@@ -21,11 +25,18 @@ public class ClassesAndObjects {
 class Person {
     String name;
     int age;
+
+    int calculateYearsRetirement() {
+        int years = 65 - age;
+        return years;
+    }
+
     void speak() {
         for (int i = 0; i < 3; i++) {
             System.out.println("Мене звати " + name + ", мені " + age);
         }
     }
+
     void sayHello() {
             System.out.println("Привіт всім!!!");
         }
