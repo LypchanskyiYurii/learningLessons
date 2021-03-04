c35a40275b02108df71ce71ab80336342177820f
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-04 13:27:33 +0200

    Add file Lessons23.java - StringBuilder

---
+++ src/com/company/Lessons23.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Lessons23 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" my").append(" friend");
+        System.out.println(sb.toString());
+    }
+
+
+
+}
