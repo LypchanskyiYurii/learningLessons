9a0afca470ad68ac671c7a82f0c672c61e390b31
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-10 13:23:27 +0200

    Add file Animal.java, Dog.java, Lessons25AnimalAndDog.java

---
+++ src/com/company/Animal.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Animal {
+    String name = "Animal's name";
+
+    public void eat () {
+        System.out.println("I am eating");
+    }
+    public void sleep () {
+        System.out.println("I am sleeping");
+    }
+}
---
+++ src/com/company/Lessons25AnimalAndDog.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Lessons25AnimalAndDog {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+
+        System.out.println("______________");
+
+        Dog Warik = new Dog();
+        Warik.eat();
+        Warik.bark();
+        Warik.sleep();
+        Warik.showName();
+
+    }
+}
