97a22557c930662b1975f5561478f1572328256a
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-19 08:56:44 +0200

    Add packet lessons34 - Wildcards

---
+++ src/lessons34/Dog.java
@@ -0,0 +1,5 @@
+package lessons34;
+
+public class Dog extends Animal {
+
+}
---
+++ src/lessons34/Test.java
@@ -0,0 +1,27 @@
+package lessons34;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Animal> listOfAnimals = new ArrayList<>();
+        listOfAnimals.add(new Animal(1));
+        listOfAnimals.add(new Animal(2));
+
+        List<Dog> listOfDog = new ArrayList<>();
+        listOfDog.add(new Dog());
+        listOfDog.add(new Dog());
+
+        test(listOfAnimals);
+        test(listOfDog);
+
+    }
+
+        private static void test(List<? extends Animal> list) {
+        for (Animal animal : list) {
+            animal.eat();
+        }
+
+    }
+}
