55622cc5dbf780cad3ae525eed6477f1ab05fd62
YuriiLipchanskiy <YuriiLipchanskiy>
2021-03-23 08:42:00 +0200

    Add

---
+++ src/lessons38/Test2.java
@@ -0,0 +1,19 @@
+package lessons38;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        try {
+            run();
+        } catch (Exception e) {
+            e.printStackTrace();
+                }
+    }
+
+    public static void run () throws IOException, ParseException, IllegalAccessException {
+
+    }
+}
