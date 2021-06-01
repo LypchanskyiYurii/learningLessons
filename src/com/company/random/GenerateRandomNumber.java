bc7e704e1b306ba98bcdee3342eb5bf5cd294cf6
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-02 00:17:57 +0300

    Bug Fixed

---
+++ src/com/company/random/GenerateRandomNumber.java
@@ -0,0 +1,15 @@
+package com.company.random;
+import java.util.*;
+
+public class GenerateRandomNumber {
+    public static void main(String[] args) {
+        int counter;
+        Random rnum = new Random();
+
+        System.out.println("Random Numbers:");
+        System.out.println("***************");
+        for (counter = 1; counter <= 5; counter++) {
+            System.out.println(rnum.nextInt(200));
+        }
+    }
+}
