788d81fe80b3d19bc7b3e13a3b10c8a67b46a6ca
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-08 15:50:44 +0300

    Change file ShowBox.java

--- src/com/company/pattern/ShowBox.java
+++ src/com/company/pattern/ShowBox.java
@@ -3,8 +3,8 @@ package com.company.pattern;
 public class ShowBox {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                if (i == 1 || i == 10 || j == 1 || j == 10)
+            for (int k = 1; k <= 10; k++) {
+                if (i == 1 || i == 10 || k == 1 || k == 10)
                     System.out.print(" 1");
                 else
                     System.out.print("  ");
