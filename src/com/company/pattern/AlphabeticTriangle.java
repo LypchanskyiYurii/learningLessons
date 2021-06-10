889c38946bd4f5373ea2d60b82135b935f843d20
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-10 23:24:34 +0300

    Add package file AlphabeticTriangle.java

---
+++ src/com/company/pattern/AlphabeticTriangle.java
@@ -0,0 +1,13 @@
+package com.company.pattern;
+
+public class AlphabeticTriangle {
+    public static void main(String[] args) {
+        int alphabet = 65;
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char) (alphabet + j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
