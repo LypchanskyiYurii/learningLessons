32b8e619c86c67ceec56e593205d22787c6514a6
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-12 08:03:55 +0300

    Change file AlphabeticTriangle.java, variable j --> k

--- src/com/company/pattern/AlphabeticTriangle.java
+++ src/com/company/pattern/AlphabeticTriangle.java
@@ -4,8 +4,8 @@ public class AlphabeticTriangle {
     public static void main(String[] args) {
         int alphabet = 65;
         for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print((char) (alphabet + j) + " ");
+            for (int k = 0; k <= i; k++) {
+                System.out.print((char) (alphabet + k) + " ");
             }
             System.out.println();
         }
