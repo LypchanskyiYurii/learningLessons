9ca71fe6a82190debb251e51a5fb138351fb5f7b
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-30 09:14:05 +0300

    Add file Decrement.java

---
+++ src/com/company/mathematics/Decrement.java
@@ -0,0 +1,15 @@
+package com.company.mathematics;
+
+public class Decrement {
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        System.out.println("Post " + "decrement = " + num--);
+
+        System.out.println("num = " + num);
+
+        System.out.println("Pre " + "decrement = " + --num);
+    }
+
+}
