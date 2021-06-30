fa4c6482f8bcaa2f35f102414c2c9050ceca01f1
Yurii <yurii.lypchanskyi@gmail.com>
2021-06-30 09:09:15 +0300

    Add file Increment.java

---
+++ src/com/company/mathematics/Increment.java
@@ -0,0 +1,13 @@
+package com.company.mathematics;
+
+public class Increment {
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        System.out.println("Post " + "increment = " + num++);
+
+        System.out.println("Pre " + "increment = " + ++num);
+    }
+
+}
--- src/com/company/pattern/printNumbers.java
+++ src/com/company/pattern/printNumbers.java
@@ -10,7 +10,6 @@ public class printNumbers {
         int rows = sc.nextInt();
 
         for (int i = 1; i <= rows; i++) {
-
             for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
