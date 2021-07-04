a7de3ba7d9694eede296cd4c7a381efb2736b1d1
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-04 11:20:59 +0300

    Add lambda to a file AddArrayAndList.java

--- src/com/company/lambda/add_array_and_list/AddArrayAndList.java
+++ src/com/company/lambda/add_array_and_list/AddArrayAndList.java
@@ -3,6 +3,7 @@ package com.company.lambda.add_array_and_list;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AddArrayAndList {
     public static void main(String[] args) {
@@ -11,6 +12,16 @@ public class AddArrayAndList {
 
         fillArr(arr);
         fillList(list);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(list);
+
+//        for (int i = 0; i < 10; i++) {
+//            arr[i] = arr[i] * 2;
+//            list.set(i, list.get(i) * 2);
+//        }
+        arr = Arrays.stream(arr).map(a -> a * 2).toArray();
+        list = list.stream().map(a -> a * 2).collect(Collectors.toList());
 
         System.out.println(Arrays.toString(arr));
         System.out.println(list);
