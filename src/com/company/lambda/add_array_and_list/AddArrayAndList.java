9bf5feafaacd9dc888066fbad0f1dd53f39546ab
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-04 10:21:51 +0300

    Add package add_array_and_list

---
+++ src/com/company/lambda/add_array_and_list/AddArrayAndList.java
@@ -0,0 +1,30 @@
+package com.company.lambda.add_array_and_list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AddArrayAndList {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        List<Integer> list = new ArrayList<>();
+
+        fillArr(arr);
+        fillList(list);
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(list);
+    }
+
+    private static void fillArr(int[] arr) {
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i + 1;
+        }
+    }
+
+    private static void fillList(List<Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 1);
+        }
+    }
+}
