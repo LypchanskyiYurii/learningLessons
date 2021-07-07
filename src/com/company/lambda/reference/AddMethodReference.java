307043891dbe6fe43e362424e9d71b8df1b67cb7
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-07 23:32:48 +0300

    Add stream to a file AddMethodReference.java

--- src/com/company/lambda/reference/AddMethodReference.java
+++ src/com/company/lambda/reference/AddMethodReference.java
@@ -1,5 +1,10 @@
 package com.company.lambda.reference;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class AddMethodReference {
     public static void main(String[] args) {
         Printer printer = new Printer() {
@@ -15,9 +20,24 @@ public class AddMethodReference {
 
         Printer methodReferencePrinter = System.out::println;
         methodReferencePrinter.print("Method reference");
+
+        Map<String, Printer> printerHub = new HashMap<>();
+        printerHub.put("lambda", lambdaPrinter);
+        printerHub.put("second", m -> {
+            System.out.println("-=-=-");
+            System.out.println(m);
+            System.out.println("-=-=-");
+        });
+        printerHub.get("second").print("Hello");
+
+        List<String> names = Arrays.asList("John", "James", "Steve", "Andrew");
+        names.stream().forEach(n -> System.out.println(n));
+        System.out.println("_________________________________");
+        names.stream().filter(n -> n.startsWith("J")).forEach(n -> System.out.println(n));
     }
 
     interface Printer {
         void print(String msg);
     }
+
 }
