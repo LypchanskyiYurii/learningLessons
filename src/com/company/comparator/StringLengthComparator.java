18b88aaa916cb9e89d70d552c5a571e760fed735
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-12 23:57:31 +0300

    Add file StringLengthComparator.java

---
+++ src/com/company/comparator/StringLengthComparator.java
@@ -0,0 +1,10 @@
+package com.company.comparator;
+
+import java.util.Comparator;
+
+public class StringLengthComparator implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return 0;
+    }
+}
