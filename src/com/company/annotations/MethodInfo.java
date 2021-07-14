e9982997e96bb5220f3c704a34984ed84a7cffb4
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-14 21:24:25 +0300

    Add two annotations to MethodInfo

--- src/com/company/annotations/MethodInfo.java
+++ src/com/company/annotations/MethodInfo.java
@@ -1,7 +1,14 @@
 package com.company.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MethodInfo {
-    String author();
-    int dateOfCreation();
+    String author() default "Yurii";
+    int dateOfCreation() default 2021;
     String purpose();
 }
