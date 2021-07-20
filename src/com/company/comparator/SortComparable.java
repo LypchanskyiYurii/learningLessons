68a4f22d5a334c7925ae66bebacb895c588bbc68
Yurii <yurii.lypchanskyi@gmail.com>
2021-07-20 21:31:02 +0300

    Add method "addElements" to a file SortComparable.java and asked to sort the two lists

--- src/com/company/comparator/SortComparable.java
+++ src/com/company/comparator/SortComparable.java
@@ -1,7 +1,26 @@
 package com.company.comparator;
 
+import java.util.*;
+
 public class SortComparable {
     public static void main(String[] args) {
+        List<Personage> peopleList = new ArrayList<>();
+        Set<Personage> peopleSet = new TreeSet<>();
 
+        addElements(peopleList);
+        addElements(peopleSet);
+
+        Collections.sort(peopleList);
+
+        System.out.println(peopleList);
+        System.out.println(peopleSet);
     }
+
+    private static void addElements(Collection collection) {
+        collection.add(new Personage(3, "Tom"));
+        collection.add(new Personage(4, "Bob"));
+        collection.add(new Personage(1, "George"));
+        collection.add(new Personage(2, "Katy"));
+    }
+
 }
