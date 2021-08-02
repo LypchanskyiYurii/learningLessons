3382ec6428d5b3e5c301ad588078baa4e479e9e4
Yurii <yurii.lypchanskyi@gmail.com>
2021-08-02 23:59:16 +0300

    Created method groupedBySpecialty to a file StreamAPIDemo.java, grouping by specialists of the specialty.

--- src/com/company/stream/StreamAPIDemo.java
+++ src/com/company/stream/StreamAPIDemo.java
@@ -1,21 +1,22 @@
 package com.company.stream;
 
 import java.util.List;
+import java.util.Map;
 
 public class StreamAPIDemo {
     public static void main(String[] args) {
         List<Specialist> specialists = StreamAPI.getSpecialists();
-        System.out.println(specialists);
+//        System.out.println(specialists);
 
-        // Filter
-        List<Specialist> engineers = StreamAPI.filterBySpecialist(specialists, Specialty.ENGINEER);
-        System.out.println(engineers);
+//        // Filter
+//        List<Specialist> engineers = StreamAPI.filterBySpecialist(specialists, Specialty.ENGINEER);
+//        System.out.println(engineers);
 
         // Sorting
-        List<Specialist> sortedSpecialistsAsc = StreamAPI.sortSpecialistsByNameAsc(specialists);
-        System.out.println(sortedSpecialistsAsc);
-        List<Specialist> sortedSpecialistsDesc = StreamAPI.sortSpecialistsByNameDesc(specialists);
-        System.out.println(sortedSpecialistsDesc);
+//        List<Specialist> sortedSpecialistsAsc = StreamAPI.sortSpecialistsByNameAsc(specialists);
+//        System.out.println(sortedSpecialistsAsc);
+//        List<Specialist> sortedSpecialistsDesc = StreamAPI.sortSpecialistsByNameDesc(specialists);
+//        System.out.println(sortedSpecialistsDesc);
 
         // Max and min salary
 //        Specialist withMaxSalary = StreamAPI.findWithMaxSalary(specialists);
@@ -23,7 +24,9 @@ public class StreamAPIDemo {
 //        Specialist withMinSalary = StreamAPI.findWithMinSalary(specialists);
 //        System.out.println(withMinSalary);
 
-        // Grouping by specialists of the specialty
+        // Grouping
+        Map<Specialty, List<Specialist>> groupedBySpecialty = StreamAPI.groupBySpecialty(specialists);
+        System.out.println(groupedBySpecialty);
     }
 
 }
