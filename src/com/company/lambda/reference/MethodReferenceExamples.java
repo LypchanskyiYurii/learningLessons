70c68659cece42e6d4adf645a9abdc35f412589b
Yurii <yurii.lypchanskyi@gmail.com>
2021-08-06 16:36:34 +0300

    Add method printSumUsingMethodReference to a file MethodReferenceExamples.java

--- src/com/company/lambda/reference/MethodReferenceExamples.java
+++ src/com/company/lambda/reference/MethodReferenceExamples.java
@@ -1,17 +1,28 @@
 package com.company.lambda.reference;
 
+import java.util.function.IntBinaryOperator;
 import java.util.function.IntUnaryOperator;
 
 public class MethodReferenceExamples {
     public static void main(String[] args) {
         printAbsUsingMethodReference(-23);
+        printSumUsingMethodReference(25,50);
 
     }
+
     private static void printAbsUsingMethodReference(int a) {
         IntUnaryOperator absOperator = Math::abs;
         int result = absOperator.applyAsInt(a);
 
         System.out.println("abs(" + a + ") = " + result);
     }
+
+    private static void printSumUsingMethodReference(int a, int b) {
+        IntBinaryOperator sumOperator = Math::addExact;
+        int result = sumOperator.applyAsInt(a, b);
+
+        System.out.println("\n" + a + " + " + b + " = " + result);
+    }
+
 
 }
