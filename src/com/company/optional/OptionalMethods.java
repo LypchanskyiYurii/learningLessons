beb68020b43b0b63c1617d334a5a7bacf799b5a0
Yurii <yurii.lypchanskyi@gmail.com>
2021-08-11 00:12:41 +0300

    Add and checked how  .orElseThrow(IllegalAccessError::new) method works

--- src/com/company/optional/OptionalMethods.java
+++ src/com/company/optional/OptionalMethods.java
@@ -4,17 +4,18 @@ import java.util.Optional;
 
 public class OptionalMethods {
     public static void main(String[] args) {
-        Optional<String> hello = Optional.ofNullable("hello");
+        Optional<String> hello = Optional.ofNullable(null);
 
         System.out.println(hello.isPresent());
         System.out.println(hello.isEmpty());
 
         String orElse = hello
                 .map(String::toUpperCase)
-                .orElseGet(() -> {
-                   // todo some logic
-                   return "world";
-                });
+                .orElseThrow(IllegalAccessError::new);
+//                .orElseGet(() -> {
+//                   // todo some logic
+//                   return "world";
+//                });
 //                .orElse("World");
         System.out.println(orElse);
     }
