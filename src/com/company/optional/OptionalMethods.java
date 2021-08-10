565bdedf7382af86c8ccdf7326383a52a1edabc0
Yurii <yurii.lypchanskyi@gmail.com>
2021-08-10 23:25:16 +0300

    Add and checked how the .isPresent() method works

---
+++ src/com/company/optional/OptionalMethods.java
@@ -0,0 +1,13 @@
+package com.company.optional;
+
+import java.util.Optional;
+
+public class OptionalMethods {
+    public static void main(String[] args) {
+        Optional<Object> empty = Optional.empty();
+
+        System.out.println(empty.isPresent());
+    }
+
+}
+
