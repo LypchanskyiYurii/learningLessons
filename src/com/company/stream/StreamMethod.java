3bb4d7e35ea0100ae28178abf03bd7129b2c89fb
Yurii <yurii.lypchanskyi@gmail.com>
2021-08-16 14:08:09 +0300

    added .skip(7) method to skip the first 7 elements

--- src/com/company/stream/StreamMethod.java
+++ src/com/company/stream/StreamMethod.java
@@ -7,15 +7,21 @@ import java.util.stream.Collectors;
 public class StreamMethod {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 1, 2, 3, 1, 2, 3);
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
         System.out.println(list);
+        System.out.println(list1);
 
         // Removing duplicates
         List<Integer> uniqueList = list.stream().distinct().collect(Collectors.toList());
         System.out.println(uniqueList);
 
         // Limit on the number of items
-        List<Integer> limitList = list.stream().limit(5).collect(Collectors.toList());
+        List<Integer> limitList = list1.stream().limit(5).collect(Collectors.toList());
         System.out.println(limitList);
+
+        // Skip the first elements
+        List<Integer> skipList = list1.stream().skip(7).collect(Collectors.toList());
+        System.out.println(skipList);
     }
 
 }
