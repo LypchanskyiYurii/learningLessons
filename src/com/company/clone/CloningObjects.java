8da68a5f47e5b486b11bd67e319fbb89224bdd64
Yurii <yurii.lypchanskyi@gmail.com>
2021-09-12 22:49:43 +0300

    added to package clone, CloningObjects.java and DollySheet.java

---
+++ src/com/company/clone/CloningObjects.java
@@ -0,0 +1,4 @@
+package com.company.clone;
+
+public class CloningObjects {
+}
---
+++ src/com/company/clone/DollySheet.java
@@ -0,0 +1,13 @@
+package com.company.clone;
+
+public class DollySheet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
