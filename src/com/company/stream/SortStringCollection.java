c5383958ede2e9f9108222f52450d610063e0ce7
Yurii <yurii.lypchanskyi@gmail.com>
2021-09-13 19:12:07 +0300

    Сreated a list of string

--- src/com/company/stream/SortStringCollection.java
+++ src/com/company/stream/SortStringCollection.java
@@ -1,7 +1,22 @@
 package com.company.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class SortStringCollection {
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<>();
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+        list.add("Four");
+        list.add("Five");
+        list.add("Six");
+        list.add("Seven");
+        list.add("Eight");
+        list.add("Nine");
+        list.add("Ten");
+        Stream stream = list.stream();
     }
 }
