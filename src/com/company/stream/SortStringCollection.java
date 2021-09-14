e37ff32cfe869f9157c7eaac20915075c9025348
Yurii <yurii.lypchanskyi@gmail.com>
2021-09-14 19:49:14 +0300

    Added to new list2<Integer>

--- src/com/company/stream/SortStringCollection.java
+++ src/com/company/stream/SortStringCollection.java
@@ -18,8 +18,22 @@ public class SortStringCollection {
         list.add("Eight");
         list.add("Nine");
         list.add("Ten");
+
 //        Stream stream = list.stream();
 //        stream.filter(x-> x.toString().length() == 3).forEach(System.out::println);
-        list.stream().sorted(Comparator.reverseOrder()).forEach(x -> System.out.println(x));
+//        list.stream().sorted().forEach(x -> System.out.println(x));
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+        list2.add(7);
+        list2.add(8);
+        list2.add(9);
+        list2.add(10);
+
     }
 }
